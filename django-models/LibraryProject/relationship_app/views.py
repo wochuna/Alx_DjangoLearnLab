3bcbf06e1a4fb3dc26cf659d846475327e00b067
wochuna <wochunayvonne@gmail.com>
2025-11-16 11:59:53 +0300

    import DetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+
 
 # Function-based view to list all books
 def list_books(request):
