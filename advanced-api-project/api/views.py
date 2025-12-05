25b55f90fb8a7ae612d99af3637c3ca84d7f99b8
wochuna <wochunayvonne@gmail.com>
2025-12-05 22:50:59 +0300

    Add app url in the project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,12 +3,20 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters
 
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['author', 'published_date']  # Filters for specific fields
+    search_fields = ['title', 'author'] # Search capabilities on these fields
+    ordering_fields = ['published_date', 'title'] # Allow ordering by these fields
+
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
