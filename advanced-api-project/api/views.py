5ae3c5f40a334cd3588f105ae84157595dbb32e7
wochuna <wochunayvonne@gmail.com>
2025-12-05 22:34:54 +0300

    Import framework permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class ListView(generics.ListAPIView):
