5b1a56182d521b058f582d12f745caafa303613b
wochuna <wochunayvonne@gmail.com>
2025-12-21 22:18:50 +0300

    urls update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from .views import register_view, login_view, logout_view, profile_view, \
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, \
-    CommentCreateView, CommentUpdateView, CommentDeleteView
+    CommentCreateView, CommentUpdateView, CommentDeleteView, PostByTagListView
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/', profile_view, name='profile'),
+    path("tags/<slug:tag_slug>/", PostByTagListView.as_view(), name="posts_by_tag"),
 ]
 
 post_urlpatterns = [
@@ -25,3 +26,5 @@ comment_urlpatterns = [
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment_update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
 ] + post_urlpatterns + urlpatterns
+
+
