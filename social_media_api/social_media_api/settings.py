6463bedab6eb08951d38ace1848f2be5311f1688
wochuna <wochunayvonne@gmail.com>
2025-12-30 23:30:14 +0300

    db credetials

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media',
+        'USER': 'root',
+        'PASSWORD': 'ChuiCube42KGBlue@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
