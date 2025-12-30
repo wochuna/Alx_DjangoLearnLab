b1578727f732a88dcc4fd3b3dfe85106da8db537
wochuna <wochunayvonne@gmail.com>
2025-12-30 23:03:36 +0300

    add the post url

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import RegistrationSerializer, LoginSerializer
+from django.shortcuts import get_object_or_404
 
 User = CustomUser
 
@@ -15,7 +16,6 @@ class RegisterView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
 
-
 class LoginView(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = LoginSerializer
@@ -25,4 +25,49 @@ class LoginView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
 
         user = serializer.validated_data['user']
-        
+
+class ProfileView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = RegistrationSerializer
+    fields = ('bio', 'profile_picture', 'followers', 'following')
+
+    def get_object(self):
+        return self.request.user
+
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+
+    def post(self, request, pk):
+        target = get_object_or_404(CustomUser, id=user_id)
+        if target == request.user:
+            return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+
+        request.user.following.add(target)
+
+        return Response(
+            {
+                "message": "followed",
+                "followed_user": {"id": target, "username": target.username}
+            },
+            status=status.HTTP_200_OK
+        )
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+
+    def post(self, request, pk):
+        target = get_object_or_404(CustomUser, id=user_id)
+        if target == request.user:
+            return Response({"detail": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+
+        request.user.following.remove(target)
+
+        return Response(
+            {
+                "message": "unfollowed",
+                "unfollowed_user": {"id": target, "username": target.username}
+            },
+            status=status.HTTP_200_OK
+        )
